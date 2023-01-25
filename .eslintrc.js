2ff970cedf4bbf512328590bd9caeb7ad9ed095e
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-01-25 14:16:01 +0900

    [#6] eslint 설정 추가 (extends airbnb rule)

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['react-app', 'react-app/jest', 'airbnb'],
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'max-depth': ['error', 2],
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+  },
+};
