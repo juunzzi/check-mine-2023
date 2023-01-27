50c153166958506a801027de05aa70525d7fc117
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-01-27 15:44:59 +0900

    [#11] DB 연동 완료 (테스트 코드 제거)

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -9,19 +9,3 @@ const pool = createPool({
 })
 
 export default pool
-
-// async function asyncFunction() {
-//     let conn
-//     try {
-//         conn = await pool.getConnection()
-//         const rows = await conn.query('SELECT 1 as val')
-//         // rows: [ {val: 1}, meta: ... ]
-
-//         const res = await conn.query('INSERT INTO myTable value (?, ?)', [1, 'mariadb'])
-//         // res: { affectedRows: 1, insertId: 1, warningStatus: 0 }
-//     } finally {
-//         if (conn) {
-//             conn.release()
-//         } // release to pool
-//     }
-// }
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,7 +2,6 @@ import './env'
 
 import Koa from 'koa'
 import koaLogger from 'koa-logger'
-import pool from 'src/db'
 
 const app = new Koa()
 
@@ -10,10 +9,6 @@ app.use(koaLogger())
 
 app.use(async (ctx) => {
     ctx.body = 'Plain Text'
-
-    console.log(pool)
-
-    pool.query('select * from product').then((data) => console.log(data))
 })
 
 app.listen(8080)
