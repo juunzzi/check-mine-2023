3500368eb9a49a6dd22fd83cdd3d66583b9861b2
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-01-30 11:03:29 +0900

    [#11] 환경 변수 필드명 수정 (host -> DB_HOST)

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,7 +1,7 @@
 import {createPool} from 'mariadb'
 
 const pool = createPool({
-    host: process.env.host,
+    host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT) ?? 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
