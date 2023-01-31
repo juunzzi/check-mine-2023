b800aad403c92d42af5257eb982ff7aa10823361
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-01-31 14:59:45 +0900

    [#20] 미드웨어 식별자 Middleware 호출 시그니처로 선언

--- packages/server/src/@domain/user/modules/middleware.ts
+++ packages/server/src/@domain/user/modules/middleware.ts
@@ -1,59 +1,57 @@
-import {DefaultContext, DefaultState, Next, ParameterizedContext} from 'koa'
+import {Middleware} from 'koa'
 import {decodeAccessToken} from 'src/@domain/user/modules/jwt'
 
-export const authenticateAccessToken =
-    () => async (ctx: ParameterizedContext<DefaultState, DefaultContext, any>, next: Next) => {
-        const {headers} = ctx.request
+export const authenticateAccessToken = (): Middleware => async (ctx, next) => {
+    const {headers} = ctx.request
 
-        try {
-            const token = headers['authorization'] && headers['authorization'].split(' ')[1]
+    try {
+        const token = headers['authorization'] && headers['authorization'].split(' ')[1]
 
-            if (!token) {
-                ctx.status = 401
-                return
-            }
-
-            const payload = await new Promise((resolve, reject) =>
-                decodeAccessToken(token, (error, decoded) => {
-                    if (error) {
-                        reject(error)
-
-                        return
-                    }
-
-                    resolve(decoded)
-                }),
-            )
-
-            ctx.request.body = payload
-
-            await next()
-        } catch (error) {
-            ctx.status = 403
+        if (!token) {
+            ctx.status = 401
+            return
         }
+
+        const payload = await new Promise((resolve, reject) =>
+            decodeAccessToken(token, (error, decoded) => {
+                if (error) {
+                    reject(error)
+
+                    return
+                }
+
+                resolve(decoded)
+            }),
+        )
+
+        ctx.request.body = payload
+
+        await next()
+    } catch (error) {
+        ctx.status = 403
     }
+}
 
-export const checkAlreadyLogin =
-    () => async (ctx: ParameterizedContext<DefaultState, DefaultContext, any>, next: Next) => {
-        const {headers} = ctx.request
+export const checkAlreadyLogin = (): Middleware => async (ctx, next) => {
+    const {headers} = ctx.request
 
-        try {
-            const token = headers['authorization'] && headers['authorization'].split(' ')[1]
+    try {
+        const token = headers['authorization'] && headers['authorization'].split(' ')[1]
 
-            await new Promise((resolve, reject) =>
-                decodeAccessToken(token, (error, decoded) => {
-                    if (decoded) {
-                        reject(decoded)
+        await new Promise((resolve, reject) =>
+            decodeAccessToken(token, (error, decoded) => {
+                if (decoded) {
+                    reject(decoded)
 
-                        return
-                    }
+                    return
+                }
 
-                    resolve(error)
-                }),
-            )
+                resolve(error)
+            }),
+        )
 
-            await next()
-        } catch (error) {
-            ctx.status = 403
-        }
+        await next()
+    } catch (error) {
+        ctx.status = 403
     }
+}
