7699642cf9e0df84f02cb4f8917038388e377d7d
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-01-31 11:40:51 +0900

    [#20] keep 파일 삭제 및 API 명세에 따른 타입 작성

---
+++ packages/server/src/@domain/user/type.ts
@@ -0,0 +1,26 @@
+export interface User {
+    id: number
+    name: string
+    email: string
+    password: string
+    payPoint: number
+    accountId: null
+}
+
+export type JoinRequestBody = Omit<User, 'accountId'>
+
+export const isJoinRequestBodyType = (body: any): body is JoinRequestBody => {
+    return (
+        body &&
+        typeof body.name === 'string' &&
+        typeof body.email === 'string' &&
+        typeof body.password === 'string' &&
+        typeof body.payPoint === 'number'
+    )
+}
+
+export type LoginRequestBody = Pick<User, 'email' | 'password'>
+
+export const isLoginRequestBodyType = (body: any): body is LoginRequestBody => {
+    return body && typeof body.email === 'string' && typeof body.password === 'string'
+}
