8c5576b74abbe46d982d4bd3a38e3a844f843fb7
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-01-31 18:14:09 +0900

    [#26] AuthenticationInfo 타입 추가 및 타입 선언 순서 변경(Router 선언 순서와 대응)

--- packages/server/src/@domain/user/type.ts
+++ packages/server/src/@domain/user/type.ts
@@ -7,6 +7,35 @@ export interface User {
     accountId: null
 }
 
+export interface AuthenticationInfo {
+    id: number
+}
+
+export const isAuthenticationInfo = (authenticationInfo: any) => {
+    return authenticationInfo && typeof authenticationInfo.id === 'number'
+}
+
+export interface MeRequestBody {
+    authenticationInfo: AuthenticationInfo
+}
+
+export const isMeRequestBodyType = (body: any): body is MeRequestBody => {
+    return body && isAuthenticationInfo(body.authenticationInfo)
+}
+
+export type EditMeRequestBodyType = Omit<User, 'password'>
+
+export const isEditMeRequestBodyType = (body: any): body is EditMeRequestBodyType => {
+    return (
+        body &&
+        body.accountId === null &&
+        typeof body.id === 'number' &&
+        typeof body.name === 'string' &&
+        typeof body.email === 'string' &&
+        typeof body.payPoint === 'number'
+    )
+}
+
 export type JoinRequestBody = Omit<User, 'accountId'>
 
 export const isJoinRequestBodyType = (body: any): body is JoinRequestBody => {
@@ -23,27 +52,4 @@ export type LoginRequestBody = Pick<User, 'email' | 'password'>
 
 export const isLoginRequestBodyType = (body: any): body is LoginRequestBody => {
     return body && typeof body.email === 'string' && typeof body.password === 'string'
-}
-
-export interface MeRequestBody {
-    decoded: {
-        id: number
-    }
-}
-
-export const isMeRequestBodyType = (body: any): body is MeRequestBody => {
-    return body && typeof body?.decoded?.id === 'number'
-}
-
-export type EditMeRequestBodyType = Omit<User, 'password'>
-
-export const isEditMeRequestBodyType = (body: any): body is EditMeRequestBodyType => {
-    return (
-        body &&
-        body.accountId === null &&
-        typeof body.id === 'number' &&
-        typeof body.name === 'string' &&
-        typeof body.email === 'string' &&
-        typeof body.payPoint === 'number'
-    )
 }
