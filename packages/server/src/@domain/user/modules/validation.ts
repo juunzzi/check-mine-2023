aa11b8640324c788fe8fa549e7a526c5840fb2b3
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-01-31 15:13:44 +0900

    [#20] name 검증 로직, 정규식으로 구현

--- packages/server/src/@domain/user/modules/validation.ts
+++ packages/server/src/@domain/user/modules/validation.ts
@@ -1,12 +1,15 @@
 import {User} from 'src/@domain/user/type'
 
+/** 영문자, 한글, 숫자만을 포함하여 2~6 글자만 이름으로 가능하다 */
+export const nameRegExp = /^[a-z|A-Z|가-힣|0-9]{2,6}$/
+
 export const emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
 
 /** 최소 8 자, 하나 이상의 영문자와 숫자 */
 export const passwordRegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
 
 export const isValidUserName = (name: string) => {
-    return name.length >= 0 && name.length < 6 && name.trim().length === name.length
+    return nameRegExp.test(name)
 }
 
 export const isValidUserEmail = (email: string) => {
