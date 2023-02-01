56ea6082ac7ca4a60060341e4bec37f2a74416f8
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-01 11:54:20 +0900

    [#27] /api 라우터에 추가

--- packages/server/src/router.ts
+++ packages/server/src/router.ts
@@ -4,7 +4,7 @@ import userRouter from 'src/@domain/user/router'
 
 const router = new Router()
 
-router.use('/users', userRouter.routes())
-router.use('/products', productRouter.routes())
+router.use('/api/users', userRouter.routes())
+router.use('/api/products', productRouter.routes())
 
 export default router
