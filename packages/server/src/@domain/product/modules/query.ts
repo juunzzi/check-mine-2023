4059d2432853a9957223fa9e1712084bc743fb28
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-03 16:56:07 +0900

    [#31] findProductsByIds Query, updateProductsStock Query 개발

--- packages/server/src/@domain/product/modules/query.ts
+++ packages/server/src/@domain/product/modules/query.ts
@@ -20,3 +20,42 @@ export const findProducts = async () => {
 
     return products
 }
+
+export const findProductsByIds = async (productIds: number[]) => {
+    const questionMark = productIds.reduce((prev, _, index) => (index === 0 ? `${prev}?` : `${prev},?`), '')
+
+    const productsQueryResult = await pool.query(`SELECT * FROM PRODUCT WHERE id IN (${questionMark})`, productIds)
+
+    const products: ProductTableRow[] = productsQueryResult.filter((element: any) => isProductTableRowType(element))
+
+    return products
+}
+
+export const updateProductsStock = async (productsWithIdAndStockOnly: Pick<Product, 'id' | 'stock'>[]) => {
+    const {idAndStocks, ids, when, questionMark} = productsWithIdAndStockOnly.reduce<{
+        idAndStocks: number[]
+        ids: number[]
+        when: string
+        questionMark: string
+    }>(
+        (prev, {id, stock}, index) => {
+            const nextWhen = `${[prev.when]}WHEN ? THEN ? `
+            const nextQuestionMark = index === 0 ? `${prev.questionMark}?` : `${prev.questionMark},?`
+
+            return {
+                idAndStocks: [...prev.idAndStocks, id, stock],
+                ids: [...prev.ids, id],
+                when: nextWhen,
+                questionMark: nextQuestionMark,
+            }
+        },
+        {idAndStocks: [], ids: [], when: '', questionMark: ''},
+    )
+
+    const productsQueryResult = await pool.query(
+        `UPDATE PRODUCT SET stock = CASE id ${when} END WHERE id IN (${questionMark})`,
+        [...idAndStocks, ...ids],
+    )
+
+    return productsQueryResult
+}
