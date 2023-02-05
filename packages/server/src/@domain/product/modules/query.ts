6ade4dfd12d494c879d2a35454b60b67bd3f21cc
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-05 23:56:49 +0900

    [#31] when, in 구문을 만들어내는 데이터베이스 유틸 함수 작성 및 적용

--- packages/server/src/@domain/product/modules/query.ts
+++ packages/server/src/@domain/product/modules/query.ts
@@ -1,4 +1,5 @@
 import {Product} from 'src/@domain/product/type'
+import {generateQueryStatement} from 'src/common/db/util'
 import pool from 'src/db'
 
 export type ProductTableRow = Product
@@ -22,9 +23,9 @@ export const findProducts = async () => {
 }
 
 export const findProductsByIds = async (productIds: number[]) => {
-    const questionMark = productIds.reduce((prev, _, index) => (index === 0 ? `${prev}?` : `${prev},?`), '')
+    const {inStatement} = generateQueryStatement(productIds.length)
 
-    const productsQueryResult = await pool.query(`SELECT * FROM PRODUCT WHERE id IN (${questionMark})`, productIds)
+    const productsQueryResult = await pool.query(`SELECT * FROM PRODUCT WHERE id ${inStatement}`, productIds)
 
     const products: ProductTableRow[] = productsQueryResult.filter((element: any) => isProductTableRowType(element))
 
@@ -32,27 +33,23 @@ export const findProductsByIds = async (productIds: number[]) => {
 }
 
 export const updateProductsStock = async (productsWithIdAndStockOnly: Pick<Product, 'id' | 'stock'>[]) => {
-    const {idAndStocks, ids, when, questionMark} = productsWithIdAndStockOnly.reduce<{
+    const {idAndStocks, ids} = productsWithIdAndStockOnly.reduce<{
         idAndStocks: number[]
         ids: number[]
-        when: string
-        questionMark: string
     }>(
-        (prev, {id, stock}, index) => {
-            const nextWhen = `${[prev.when]}WHEN ? THEN ? `
-            const nextQuestionMark = index === 0 ? `${prev.questionMark}?` : `${prev.questionMark},?`
-
+        (prev, {id, stock}) => {
             return {
                 idAndStocks: [...prev.idAndStocks, id, stock],
                 ids: [...prev.ids, id],
-                when: nextWhen,
-                questionMark: nextQuestionMark,
             }
         },
-        {idAndStocks: [], ids: [], when: '', questionMark: ''},
+        {idAndStocks: [], ids: []},
     )
+
+    const {inStatement, whenStatement} = generateQueryStatement(productsWithIdAndStockOnly.length)
+
     const productsQueryResult = await pool.query(
-        `UPDATE PRODUCT SET stock = CASE id ${when} END WHERE id IN (${questionMark})`,
+        `UPDATE PRODUCT SET stock = CASE id ${whenStatement} END WHERE id ${inStatement}`,
         [...idAndStocks, ...ids],
     )
 
--- packages/server/src/common/db/util.ts
+++ packages/server/src/common/db/util.ts
@@ -18,3 +18,21 @@ export const transactQueries = async <T>(executor: (connection: PoolConnection) 
         throw error
     }
 }
+
+export const generateQueryStatement = (iterationCount: number) => {
+    return Array.from({length: iterationCount}).reduce<{inStatement: string; whenStatement: string}>(
+        (prev, _, index) => {
+            const nextInStatement = index === 0 ? `${prev.inStatement}?` : `${prev.inStatement},?`
+            const nextWhenStatement = `${prev.whenStatement}WHEN ? THEN ? `
+
+            return {
+                inStatement: nextInStatement,
+                whenStatement: nextWhenStatement,
+            }
+        },
+        {
+            inStatement: '',
+            whenStatement: '',
+        },
+    )
+}
