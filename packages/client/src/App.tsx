8ff789bfebaab085a3111331c5666aff4d7975af
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-07 17:06:37 +0900

    [#52] react query 초기 설정

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,16 +1,33 @@
 import {Global, ThemeProvider} from '@emotion/react'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {BrowserRouter} from 'react-router-dom'
 import Router from 'src/Router'
 import globalStyle from 'src/styles/globalStyle'
 import theme from 'src/styles/theme'
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            suspense: true,
+            retry: false,
+            useErrorBoundary: true,
+            staleTime: Infinity,
+        },
+        mutations: {
+            useErrorBoundary: true,
+        },
+    },
+})
+
 const App = () => {
     return (
         <BrowserRouter>
-            <ThemeProvider theme={theme}>
-                <Global styles={globalStyle} />
-                <Router />
-            </ThemeProvider>
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider theme={theme}>
+                    <Global styles={globalStyle} />
+                    <Router />
+                </ThemeProvider>
+            </QueryClientProvider>
         </BrowserRouter>
     )
 }
