433c13784079f10a3452e8509a44705d20ecd1e4
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-08 14:10:32 +0900

    [#58] ToastItem 컴포넌트 개발

---
+++ packages/client/src/@components/commons/Toast/Item/index.tsx
@@ -0,0 +1,55 @@
+import {useEffect, useRef} from 'react'
+import {TOAST_DELAY} from 'src/@components/commons/Toast/constant'
+import {useProgress} from 'src/@components/commons/Toast/hooks'
+import {Toast} from 'src/@components/commons/Toast/type'
+
+import * as Styled from './style'
+
+interface ToastItemProps extends Toast {
+    deleteItem: (id: string) => void
+    deactiveItem: (id: string) => void
+}
+
+const ToastItem = (props: ToastItemProps) => {
+    const {id, message, status, isActive, deleteItem, deactiveItem} = props
+
+    const toastElement = useRef<HTMLDivElement>(null)
+
+    const progress = useProgress(TOAST_DELAY)
+
+    const onClickToastItem = () => {
+        deactiveItem(id)
+    }
+
+    useEffect(() => {
+        if (progress < 100) {
+            return
+        }
+
+        deactiveItem(id)
+    }, [progress])
+
+    useEffect(() => {
+        if (isActive || !toastElement.current) {
+            return
+        }
+
+        toastElement.current.getAnimations().forEach((animation) => {
+            animation.onfinish = () => {
+                deleteItem(id)
+            }
+        })
+    }, [isActive])
+
+    return (
+        <Styled.Container ref={toastElement} status={status} isActive={isActive}>
+            <Styled.Close onClick={onClickToastItem}>x</Styled.Close>
+            <Styled.Message>{message}</Styled.Message>
+            <Styled.ProgressContainer>
+                <Styled.Progress progress={progress} />
+            </Styled.ProgressContainer>
+        </Styled.Container>
+    )
+}
+
+export default ToastItem
---
+++ packages/client/src/@components/commons/Toast/Item/style.tsx
@@ -0,0 +1,80 @@
+import {css} from '@emotion/react'
+import styled from '@emotion/styled'
+import {ToastStatus} from 'src/@components/commons/Toast/type'
+
+export const Container = styled.div<{status: ToastStatus; isActive: boolean}>`
+    position: relative;
+
+    width: 200px;
+
+    padding: 20px 10px;
+
+    border-radius: 4px;
+
+    ${({theme}) => css`
+        color: ${theme.colors.white_100};
+    `}
+
+    ${({status, theme}) => {
+        if (status === 'error') {
+            return css`
+                background-color: ${theme.colors.red_800};
+            `
+        }
+
+        if (status === 'success') {
+            return css`
+                background-color: ${theme.colors.green_500};
+            `
+        }
+
+        if (status === 'warning') {
+            return css`
+                background-color: ${theme.colors.orange_500};
+            `
+        }
+    }}
+
+    ${({isActive}) => {
+        return isActive
+            ? css`
+                  animation: fade-in 1.2s ease-in both;
+              `
+            : css`
+                  animation: fade-out 1.2s ease-in both;
+              `
+    }}
+`
+
+export const Message = styled.div`
+    overflow-x: scroll;
+
+    padding: 5px;
+`
+
+export const Close = styled.div`
+    position: absolute;
+    top: 2px;
+    right: 5px;
+
+    cursor: pointer;
+`
+
+export const ProgressContainer = styled.div`
+    width: 190px;
+
+    position: absolute;
+    left: 50%;
+    bottom: 2px;
+    transform: translateX(-50%);
+
+    background-color: white;
+`
+
+export const Progress = styled.div<{progress: number}>`
+    ${({progress, theme}) => css`
+        width: ${progress}%;
+
+        border: 1.5px solid ${theme.colors.light_grey_200};
+    `}
+`
