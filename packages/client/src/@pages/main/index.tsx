49627770e36a29866a7a125c990221b2e32fab7d
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-08 14:14:24 +0900

    [#58] Main Page의 렌더링 테스트 코드 삭제

--- packages/client/src/@pages/main/index.tsx
+++ packages/client/src/@pages/main/index.tsx
@@ -1,13 +1,4 @@
-import {useEffect} from 'react'
-import {useToast} from 'src/@components/commons/Toast/hooks'
-
 const MainPage = () => {
-    const {showToastMessage} = useToast()
-
-    useEffect(() => {
-        showToastMessage('hi', 'error')
-    }, [])
-
     return <div>MainPage</div>
 }
 
