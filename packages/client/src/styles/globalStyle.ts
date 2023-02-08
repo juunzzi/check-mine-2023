dddde67948a3f9a84246ee99cac35f3db190898c
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-08 14:12:59 +0900

    [#58] globalStyle에 fade-in, fade-out 애니메이션 추가

--- packages/client/src/styles/globalStyle.ts
+++ packages/client/src/styles/globalStyle.ts
@@ -53,6 +53,24 @@ const globalStyle = css`
     input[type='number'] {
         -moz-appearance: textfield;
     }
+
+    @keyframes fade-in {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    @keyframes fade-out {
+        0% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
 `
 
 export default globalStyle
