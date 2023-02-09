2c17adc35f3df5e438b048c8cdb8761ff9eacb47
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-09 16:03:22 +0900

    [#60)] Join Page 스타일링

--- packages/client/src/@components/user/UserJoinForm/index.tsx
+++ packages/client/src/@components/user/UserJoinForm/index.tsx
@@ -1,4 +1,6 @@
 import {FormEventHandler} from 'react'
+import Button from 'src/@components/common/Button'
+import Input from 'src/@components/common/Input'
 import {useUserJoinForm} from 'src/@components/user/UserJoinForm/hooks'
 import {JoinUserRequestBody} from 'src/@domain/api/user'
 import {ChangeUserInputArgs} from 'src/@pages/join'
@@ -19,11 +21,40 @@ const UserJoinForm = (props: UserJoinFormProps) => {
 
     return (
         <Styled.FormConainer onSubmit={submitUserJoinForm}>
-            <input value={name} onChange={onChangeNameInput} placeholder="이름을 입력해주세요" />
-            <input value={email} onChange={onChangeEmailInput} placeholder="이메일을 입력해주세요" />
-            <input value={password} onChange={onChangePasswordInput} placeholder="비밀번호를 입력해주세요" />
-            <input value={payPoint} onChange={onChangePayPointInput} placeholder="페이 포인트를 입력해주세요" />
-            <button>제출</button>
+            <Input
+                value={name}
+                onChange={onChangeNameInput}
+                placeholder="이름을 입력해주세요 (2~6자)"
+                label="이름 (2~6자)"
+            />
+            <Input
+                type="email"
+                value={email}
+                onChange={onChangeEmailInput}
+                placeholder="이메일을 입력해주세요"
+                label="이메일"
+            />
+            <Input
+                type="password"
+                value={password}
+                onChange={onChangePasswordInput}
+                placeholder="비밀번호를 입력해주세요 (최소 8자, 하나 이상의 영문자)"
+                label="비밀번호 (최소 8자, 하나 이상의 영문자)"
+            />
+            <Input
+                type="password"
+                value={password}
+                onChange={onChangePasswordInput}
+                placeholder="비밀번호를 재입력해주세요"
+                label="비밀번호 확인"
+            />
+            <Input
+                value={payPoint}
+                onChange={onChangePayPointInput}
+                placeholder="페이 포인트를 입력해주세요"
+                label="페이 포인트"
+            />
+            <Button>제출</Button>
         </Styled.FormConainer>
     )
 }
--- packages/client/src/@pages/join/index.tsx
+++ packages/client/src/@pages/join/index.tsx
@@ -1,6 +1,7 @@
 import PageTemplate from 'src/@components/common/PageTemplate'
 import UserJoinForm from 'src/@components/user/UserJoinForm'
 import useUserJoinFormPage from 'src/@pages/join/hooks'
+import {PATH} from 'src/Router'
 
 import * as Styled from './style'
 
@@ -15,11 +16,13 @@ const JoinPage = () => {
     return (
         <PageTemplate>
             <Styled.Container>
+                <Styled.FormLabel>회원가입</Styled.FormLabel>
                 <UserJoinForm
                     changeUserInput={changeUserInput}
                     submitUserJoinForm={submitUserJoinForm}
                     {...userInput}
                 />
+                <Styled.LoginLink to={PATH.LOGIN}>이미 회원이신가요?</Styled.LoginLink>
             </Styled.Container>
         </PageTemplate>
     )
