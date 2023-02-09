727c7c1b420152e4860dc931bef3dbbe33d035a9
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-09 19:07:19 +0900

    [#60] let 키워드로 수정 (avoidRepeatRequest)

--- packages/client/src/common/util/func.ts
+++ packages/client/src/common/util/func.ts
@@ -1,24 +1,20 @@
 import {RES_MSG} from 'payment_common/module/constant'
 
 export const avoidRepeatRequest = <T extends unknown[], R>(api: (...args: T) => Promise<R>) => {
-    const isPending: {
-        current: boolean
-    } = {
-        current: false,
-    }
+    let isPending = false
 
     return async (...args: T) => {
-        if (isPending.current) {
+        if (isPending) {
             alert('요청을 중복으로 보낼 수는 없습니다.')
 
             return {message: RES_MSG.FAILURE}
         }
 
-        isPending.current = true
+        isPending = true
 
         const data = await api(...args)
 
-        isPending.current = false
+        isPending = false
 
         return data
     }
