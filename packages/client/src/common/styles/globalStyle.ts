264205e6b87d802124ac2a9a32d2d2bcf63f233b
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-09 16:01:39 +0900

    [#60)] global font color 스타일 수정 (테마 색상으로 적용)

--- packages/client/src/common/styles/globalStyle.ts
+++ packages/client/src/common/styles/globalStyle.ts
@@ -1,12 +1,12 @@
-import {css} from '@emotion/react'
+import {css, Theme} from '@emotion/react'
 
-const globalStyle = css`
+const globalStyle = (theme: Theme) => css`
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         font: inherit;
-        color: inherit;
+        color: ${theme.colors.grey_400};
         flex-shrink: 0;
     }
 
