855ce6b38f308ef573c021a795452b695be5b59c
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-09 15:27:16 +0900

    [#60] Icon 컴포넌트 개발

---
+++ packages/client/src/@components/common/Icon/index.tsx
@@ -0,0 +1,6 @@
+import Icon from 'src/@components/common/Icon/SvgIcon'
+
+export {ReactComponent as userIcon} from './icons/icon-user.svg'
+export {ReactComponent as mainIcon} from './icons/icon-main.svg'
+
+export default Icon
