d30efbbc6bb2cadc2edcba4d910d140c32ade321
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-10 14:00:34 +0900

    [#55] UserLogin 로직 개발

--- packages/client/src/@domain/api/user.ts
+++ packages/client/src/@domain/api/user.ts
@@ -2,10 +2,21 @@ import {client} from 'src/@domain/api'
 import {User} from 'src/@domain/types/user'
 
 export type JoinUserRequestBody = Omit<User, 'id' | 'accountId'>
+export type JoinUserResponseBody = undefined
+
+export type LoginUserRequestBody = Pick<User, 'email' | 'password'>
+export interface LoginUserResponseBody {
+    data: {accessToken: string}
+}
 
 const USER_API = {
     join: (body: JoinUserRequestBody) => {
-        return client.post<undefined>('/users/join', body)
+        return client.post<JoinUserResponseBody>('/users/join', body)
+    },
+    login: async (body: LoginUserRequestBody) => {
+        const {data} = await client.post<LoginUserResponseBody>('/users/login', body)
+
+        return data
     },
 }
 
--- packages/client/src/@domain/hooks/user.ts
+++ packages/client/src/@domain/hooks/user.ts
@@ -2,7 +2,7 @@ import {RES_MSG} from 'payment_common/module/constant'
 import {useLoading} from 'src/@components/common/Loading/hooks'
 import {useToast} from 'src/@components/common/Toast/hooks'
 import {hasAxiosResponseAxiosErrorType, hasErrorMessageAxiosResponseType} from 'src/@domain/api'
-import USER_API, {JoinUserRequestBody} from 'src/@domain/api/user'
+import USER_API, {JoinUserRequestBody, LoginUserRequestBody} from 'src/@domain/api/user'
 import {avoidRepeatRequest} from 'src/common/util/func'
 
 export const useMutateUserDomain = () => {
@@ -51,7 +51,53 @@ export const useMutateUserDomain = () => {
         }
     }
 
+    const loginUser = async (args: LoginUserRequestBody) => {
+        try {
+            showLoading()
+
+            const {
+                data: {accessToken},
+            } = await USER_API.login(args)
+
+            localStorage.setItem('user-authorization-token', accessToken)
+
+            showToastMessage('회원가입에 성공하였습니다', 'success')
+
+            return {
+                data: {
+                    accessToken,
+                },
+                message: RES_MSG.SUCCESS,
+            }
+        } catch (error) {
+            if (!hasAxiosResponseAxiosErrorType(error) || !hasErrorMessageAxiosResponseType(error.response)) {
+                showToastMessage('알 수 없는 에러가 발생하였습니다.', 'error')
+
+                return {
+                    message: RES_MSG.FAILURE,
+                }
+            }
+
+            const {
+                response: {
+                    data: {message},
+                },
+            } = error
+
+            if (message === RES_MSG.INPUT_TYPE_ERROR) {
+                showToastMessage('입력 값이 올바르지 않습니다.', 'error')
+            }
+
+            return {
+                message: RES_MSG.FAILURE,
+            }
+        } finally {
+            hideLoading()
+        }
+    }
+
     return {
         joinUser: avoidRepeatRequest(joinUser),
+        loginUser: avoidRepeatRequest(loginUser),
     }
 }
