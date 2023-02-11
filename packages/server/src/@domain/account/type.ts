8c150131496878a6d562c36cf5ad617d1e2be7ec
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-11 19:41:26 +0900

    [#56] payment_common으로 계좌 관련 상수 및 검증 로직 추출 작업에 따른 변경사항 반영(서버 단)

--- packages/server/src/@domain/account/type.ts
+++ packages/server/src/@domain/account/type.ts
@@ -1,9 +1,8 @@
-import {isValidBankName} from 'src/@domain/account/modules/validation'
+import {Bank} from 'payment_common/module/constant'
+import {isValidBankName} from 'payment_common/module/validation'
 import {AuthenticationInfo, isAuthenticationInfoType} from 'src/@domain/user/type'
 import {isNumberType, isStringType} from 'src/common/type/guard'
-export const BANK = ['국민은행', '신한은행', '토스뱅크'] as const
 
-export type Bank = (typeof BANK)[number]
 export interface Account {
     id: number
     bankName: Bank
