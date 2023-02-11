ed9d6f57722ac2d676ae65fe47737c4d137694a3
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-12 01:10:21 +0900

    [#56] Eye, EyeClose Icon 추가

--- packages/client/src/@components/common/Icon/index.tsx
+++ packages/client/src/@components/common/Icon/index.tsx
@@ -2,5 +2,7 @@ import Icon from 'src/@components/common/Icon/SvgIcon'
 
 export {ReactComponent as UserIcon} from './icons/icon-user.svg'
 export {ReactComponent as LogoIcon} from './icons/icon-logo.svg'
+export {ReactComponent as EyeIcon} from './icons/icon-eye.svg'
+export {ReactComponent as EyeCloseIcon} from './icons/icon-eye-close.svg'
 
 export default Icon
