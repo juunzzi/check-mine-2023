79cb99541f0fd391c2c8c55683a907c7352e1e3f
junhyeok-jang8397 <junhyeok.jang8397@navercorp.com>
2023-02-20 17:43:15 +0900

    [#82] X Icon 추가

--- packages/client/src/@components/common/Icon/index.tsx
+++ packages/client/src/@components/common/Icon/index.tsx
@@ -6,5 +6,6 @@ export {ReactComponent as EyeIcon} from './icons/icon-eye.svg'
 export {ReactComponent as EyeCloseIcon} from './icons/icon-eye-close.svg'
 export {ReactComponent as ReloadIcon} from './icons/icon-reload.svg'
 export {ReactComponent as MainIcon} from './icons/icon-main.svg'
+export {ReactComponent as XIcon} from './icons/icon-x.svg'
 
 export default Icon
